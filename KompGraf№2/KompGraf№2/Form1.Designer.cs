f76e5889e7d638fcf6eacc67643900811a24064c
NikitBogdanov <92532985+NikitBogdanov@users.noreply.github.com>
2022-10-13 16:23:25 +0300

    KompGraf-2

---
+++ KompGraf№2/KompGraf№2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace KompGraf_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Pixel = new System.Windows.Forms.Button();
+            this.Millimeter = new System.Windows.Forms.Button();
+            this.Inch = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Pixel
+            // 
+            this.Pixel.Location = new System.Drawing.Point(12, 493);
+            this.Pixel.Name = "Pixel";
+            this.Pixel.Size = new System.Drawing.Size(167, 46);
+            this.Pixel.TabIndex = 1;
+            this.Pixel.Text = "Pixel";
+            this.Pixel.UseVisualStyleBackColor = true;
+            this.Pixel.Click += new System.EventHandler(this.Pixel_Click);
+            // 
+            // Millimeter
+            // 
+            this.Millimeter.Location = new System.Drawing.Point(254, 493);
+            this.Millimeter.Name = "Millimeter";
+            this.Millimeter.Size = new System.Drawing.Size(167, 46);
+            this.Millimeter.TabIndex = 1;
+            this.Millimeter.Text = "Millimeter";
+            this.Millimeter.UseVisualStyleBackColor = true;
+            this.Millimeter.Click += new System.EventHandler(this.Millimeter_Click);
+            // 
+            // Inch
+            // 
+            this.Inch.Location = new System.Drawing.Point(500, 493);
+            this.Inch.Name = "Inch";
+            this.Inch.Size = new System.Drawing.Size(167, 46);
+            this.Inch.TabIndex = 1;
+            this.Inch.Text = "Inch";
+            this.Inch.UseVisualStyleBackColor = true;
+            this.Inch.Click += new System.EventHandler(this.Inch_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(745, 493);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(167, 46);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Очистка";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 570);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Inch);
+            this.Controls.Add(this.Millimeter);
+            this.Controls.Add(this.Pixel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Pixel;
+        private System.Windows.Forms.Button Millimeter;
+        private System.Windows.Forms.Button Inch;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ KompGraf№2/KompGraf№2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Drawing.Printing;
+
+namespace KompGraf_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.FromKnownColor(KnownColor.AliceBlue);
+        }
+        private void Pixel_Click(object sender, EventArgs e)
+        {
+            DrawSystem();
+
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.FromArgb(9, 167, 172), 3f);
+            graphics.PageUnit = GraphicsUnit.Pixel;
+
+            Random random = new Random();
+
+            Point p1 = new Point(pictureBox1.Width/2 + random.Next(-100, 100), pictureBox1.Height/2 + (3 * random.Next(-100, 100) + 1));
+            Point p2 = new Point(pictureBox1.Width / 2 + random.Next(-100, 100), pictureBox1.Height / 2 + (3 * random.Next(-100, 100) + 1));
+            graphics.DrawLine(pen, p1, p2);
+        }
+
+        private void Millimeter_Click(object sender, EventArgs e)
+        {
+            DrawSystem();
+
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.FromArgb(185, 41, 5), 1f);
+            graphics.PageUnit = GraphicsUnit.Millimeter;
+
+            Random random = new Random();
+
+            int widthCentr = (int)((pictureBox1.Width / 2) / 3.793627);
+            int heightCentr = (int)((pictureBox1.Height / 2) / 3.793627);
+            Point p1 = new Point(widthCentr + (int)(random.Next(-100, 100) / 3.793627), heightCentr + (int)((3 * random.Next(-100, 100) + 1) / 3.793627));
+            Point p2 = new Point(widthCentr + (int)(random.Next(-100, 100) / 3.793627), heightCentr + (int)((3 * random.Next(-100, 100) + 1) / 3.793627));
+            graphics.DrawLine(pen, p1, p2);
+        }
+
+        private void Inch_Click(object sender, EventArgs e)
+        {
+            DrawSystem();
+
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.FromArgb(98, 6, 121), 0.05f);
+            graphics.PageUnit = GraphicsUnit.Inch;
+
+            Random random = new Random();
+
+            int widthCentr = (int)((pictureBox1.Width / 2) / 96.358115);
+            int heightCentr = (int)((pictureBox1.Height / 2) / 96.358115);
+            Point p1 = new Point(widthCentr + (int)(random.Next(-200, 200) / 96.358115), heightCentr + (int)((3 * random.Next(-200, 200) + 1) / 96.358115));
+            Point p2 = new Point(widthCentr + (int)(random.Next(-200, 200) / 96.358115), heightCentr + (int)((3 * random.Next(-200, 200) + 1) / 96.358115));
+            graphics.DrawLine(pen, p1, p2);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.FromKnownColor(KnownColor.AliceBlue));
+        }
+
+        private void DrawSystem()
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen myPen = new Pen(Color.Black, 2f);
+            graphics.DrawLine(myPen, new Point(pictureBox1.Width / 2, 0), new Point(pictureBox1.Width / 2, pictureBox1.Height));
+            graphics.DrawLine(myPen, new Point(0, pictureBox1.Height / 2), new Point(pictureBox1.Width, pictureBox1.Height / 2));
+            graphics.DrawRectangle(myPen, 0, 0, pictureBox1.Size.Width - 1, pictureBox1.Size.Height - 1);
+        }
+    }
+}
